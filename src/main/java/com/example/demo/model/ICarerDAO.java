ad2b1a7b48e306806996943202311abceb509b95
Deepu <deepu.joy@connect.qut.edu.au>
2024-09-11 16:21:21 +1000

    Carer and Staff Objects created for DB Access

--- src/main/java/com/example/demo/model/ICarerDAO.java
+++ src/main/java/com/example/demo/model/ICarerDAO.java
@@ -1,4 +1,10 @@
 package com.example.demo.model;
 
 public interface ICarerDAO {
+    // Create table (real DAO will implement this to create the table in the DB)
+    void createTable();
+
+    // Method for carer login
+    Carer login(String email, String password);
 }
+
--- src/main/java/com/example/demo/model/IStaffDAO.java
+++ src/main/java/com/example/demo/model/IStaffDAO.java
@@ -1,4 +1,8 @@
 package com.example.demo.model;
 
 public interface IStaffDAO {
+    void createTable();
+
+    // Method for carer login
+    Staff login(String email, String password);
 }
