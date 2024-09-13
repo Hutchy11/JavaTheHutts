ac77400385a87843f640a74ddd42dd3e4d7880fd
luke-lyon <113181281+luke-lyon@users.noreply.github.com>
2024-09-13 10:36:58 +1000

    Update InitialStaffViewController.java
    Initial Form to enter child data

--- src/main/java/com/example/demo/controller/InitialStaffViewController.java
+++ src/main/java/com/example/demo/controller/InitialStaffViewController.java
@@ -3,7 +3,15 @@ package com.example.demo.controller;
 import com.example.demo.model.Staff;
 import com.example.demo.model.Session;
 import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import javafx.scene.control.Label;
+
+import java.io.IOException;
+
 
 public class InitialStaffViewController {
 
@@ -11,15 +19,29 @@ public class InitialStaffViewController {
     private Label successLabel;
 
     public void initialize() {
-        Staff loggedStaff = Session.getLoggedStaff();  // Retrieve the carer from the session
+        Staff loggedStaff = Session.getLoggedStaff();  // Retrieve the Staff from the session
 
         if (loggedStaff != null) {
-            // Set the label with the carer's full name and email
-            successLabel.setText("Successful Carer Login: "
+            // Set the label with the Staff's full name and email
+            successLabel.setText("Successful Staff Login: "
                     + loggedStaff.getFullName()
                     + " (" + loggedStaff.getEmail() + ")");
         } else {
             successLabel.setText("No Staff Logged In.");
+        }
+    }
+
+
+    @FXML
+    private void createChildProfile() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/demo/ChildProfileForm.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Create Child Profile");
+            stage.setScene(new Scene(fxmlLoader.load()));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
