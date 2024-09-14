1d6057f8cc017464d14e67c4c7b2eac33f7c6db1
luke-lyon <113181281+luke-lyon@users.noreply.github.com>
2024-09-15 08:55:23 +1000

    Update ICarerDAO.java
    Interface Update

--- src/main/java/com/example/demo/model/ICarerDAO.java
+++ src/main/java/com/example/demo/model/ICarerDAO.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 public interface ICarerDAO {
     // Create table (real DAO will implement this to create the table in the DB)
     void createTable();
 
     // Method for carer login
     Carer login(String email, String password);
+
+    List<Carer> getAllCarers();
 }
 
