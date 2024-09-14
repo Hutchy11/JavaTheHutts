b94f25d71772434fa58045bd033f60c41a6bf233
luke-lyon <113181281+luke-lyon@users.noreply.github.com>
2024-09-15 08:44:59 +1000

    Update IChildDAO.java
    updated interface

--- src/main/java/com/example/demo/model/IChildDAO.java
+++ src/main/java/com/example/demo/model/IChildDAO.java
@@ -1,6 +1,12 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 public interface IChildDAO {
     // Create table (real DAO will implement this to create the table in the DB)
     void createTable();
+
+    void insertChild(Child child);
+
+    List<Child> getAllChildren();
 }
