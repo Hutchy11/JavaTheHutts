673ad634abafa8ff1b35eace2fe0afde1b4d866c
n8429693 <n8429693@qut.edu.au>
2024-09-15 15:51:48 +1000

    Created controller for initial staff view.

---
+++ src/main/java/com/example/demo/controller/InitialStaffViewController.java
@@ -0,0 +1,46 @@
+ package com.example.demo.controller;
+
+import com.example.demo.model.Carer;
+import com.example.demo.model.Session;
+import com.example.demo.model.Staff;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+// Understand the elements used in the import and reuse in the screen.
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class InitialStaffViewController {
+
+    @FXML
+    private Label successLabel;
+
+    public void initialize() {
+        Staff loggedStaff = Session.getLoggedStaff();  // Retrieve the carer from the session
+
+        if (loggedStaff != null) {
+            // Set the label with the carer's full name and email
+            successLabel.setText("Successful Staff Login: "
+                    + loggedStaff.getFullName()
+                    + " (" + loggedStaff.getEmail() + ")");
+        } else {
+            successLabel.setText("No Staff Logged In.");
+        }
+    }
+
+    @FXML
+    private void navigateToHome(ActionEvent event) {
+        // Implement the navigation logic here
+        System.out.println("Navigating to Home");
+    }
+}
